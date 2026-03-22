1f882f1b49cdd6ce34ae2d186bc907a633749bb6
Shraddha Gupta <84114925+shraddha-crpto@users.noreply.github.com>
2026-03-21 20:20:54 -0400

    added missing indicators and their subsequent features

--- sg_preprocessing/extract_7subseq_features.py
+++ sg_preprocessing/extract_7subseq_features.py
@@ -61,14 +61,16 @@ def compute_stats(values):
 # ----------------------------------------
 # Extract features for one patient
 # ----------------------------------------
-
+# 17 variables × 7 subsequences × 6 stats = 714
+# 17 variables x 7 subsequences (missing flags) = 119
+# 714 (stats)+ 119 (subsequence missing flags)= 833+ label = 834
 def extract_patient_features(file_path):
 
     df = pd.read_csv(file_path)
 
     df = df.sort_values("Hours")
 
-    variables = [c for c in df.columns if c != "Hours"]
+    variables = [c for c in df.columns if c != "Hours" and not c.endswith("_missing")]
 
     features = {}
 
@@ -82,11 +84,14 @@ def extract_patient_features(file_path):
 
             stats = compute_stats(subseq)
 
+            # Add statistical features (UNCHANGED)
             for stat_name, val in stats.items():
-
                 feature_name = f"{var}_{name}_{stat_name}"
-
                 features[feature_name] = val
+
+            # Add missing indicator (NEW)
+            missing_flag = int(subseq.dropna().shape[0] == 0)
+            features[f"{var}_{name}_missing"] = missing_flag
 
     return features
 
